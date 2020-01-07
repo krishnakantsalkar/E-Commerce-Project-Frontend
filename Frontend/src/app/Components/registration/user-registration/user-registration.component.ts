c919fa6123073aeb0a5b74d02f15e1f7d58f2f35
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-08 00:55:50 +0530

    #Frontend: User Registration Page

--- Frontend/src/app/Components/registration/user-registration/user-registration.component.ts
+++ Frontend/src/app/Components/registration/user-registration/user-registration.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 import { IuserReg } from "../../../Shared/Model/usersRegistration";
 import { userRegisterData } from "../../../Shared/Services/userRegisterService";
 import { Router } from "@angular/router";
+import { HttpResponse, HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: "app-user-registration",
@@ -10,20 +11,23 @@ import { Router } from "@angular/router";
   styleUrls: ["./user-registration.component.css"]
 })
 export class UserRegistrationComponent implements OnInit {
-  public userForm: FormGroup;
-  public userRegData: IuserReg;
-  public router: Router;
+  public userForm: FormGroup; // Main formGroup
+  public userRegData: IuserReg; // Interface correlating to DB
+  public error: any; // error handling to UI
+
   constructor(
-    private fb: FormBuilder,
-    private registerService: userRegisterData
+    private fb: FormBuilder, // dependancy injection to form builder
+    private registerService: userRegisterData, //  dependancy injection to Register Services
+    private router: Router //  dependancy injection to Router
   ) {}
 
   ngOnInit() {
     this.userForm = this.fb.group({
+      //main form group
       firstname: [
         "",
         [
-          Validators.required,
+          Validators.required, ////validations
           Validators.minLength(5),
           Validators.maxLength(100)
         ]
@@ -38,19 +42,30 @@ export class UserRegistrationComponent implements OnInit {
       ],
       newsLetterCheck: ["", Validators.required],
       userLogin: this.fb.group({
+        //sub form group
         userEmail: ["", [Validators.required, Validators.email]],
         userPassword: ["", [Validators.required]]
       }),
       termsAcceptCheck: ["", [Validators.required]]
-      // sendConfirmationMail?: []
+      // sendConfirmationMail?: []          // optional send confirmation mail check backend APIs
     });
   }
+
   Save(data) {
+    // Save function on Submit in UI
     console.log(data);
-    this.registerService.Register(data).subscribe(item => {
-      alert("Registration successfull!");
-      this.router.navigateByUrl("/Home");
-      console.log(item);
-    });
+    this.registerService.Register(data).subscribe(
+      // services usage
+      item => {
+        alert("Registration successfull!");
+        this.router.navigateByUrl("/Home");
+        console.log(item);
+      },
+      error => {
+        //error handling to be shown to user
+        console.log(error);
+        alert("Check entered data or Email Id alredy exists");
+      }
+    );
   }
 }
