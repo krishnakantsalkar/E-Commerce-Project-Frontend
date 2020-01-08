3ea346eedbcb70ff88e8543b707a6a05b8566b6a
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-08 13:25:26 +0530

    #Frontend: User Login Page

--- Frontend/src/app/Components/login/user-login/user-login.component.ts
+++ Frontend/src/app/Components/login/user-login/user-login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { userLoginData } from "../../../Shared/Services/userLoginService";
 import { IuserLogin } from "../../../Shared/Model/userLogin";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Router } from "@angular/router";
 
 @Component({
   selector: "app-user-login",
@@ -10,21 +11,37 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 })
 export class UserLoginComponent implements OnInit {
   public newLogin: FormGroup;
-  constructor(private UL: userLoginData, private fb: FormBuilder) {}
+  public submitted: boolean;
+  constructor(
+    private UL: userLoginData,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.newLogin = this.fb.group({
       userLogin: this.fb.group({
         userEmail: ["", [Validators.required, Validators.email]],
-        userPassword: ["", [Validators.required]]
+        userPassword: ["", [Validators.required, Validators.minLength(8)]]
       })
     });
   }
-  Save(data) {
+  Save(data: IuserLogin) {
+    this.submitted = true;
+    if (!this.newLogin.valid) {
+      return;
+    }
     console.log(data);
-    this.UL.Login(data).subscribe(item => {
-      alert("login successful!");
-      console.log(item);
-    });
+    this.UL.Login(data).subscribe(
+      item => {
+        alert("login successful!");
+        this.router.navigateByUrl("/Home");
+        console.log(item);
+      },
+      error => {
+        alert("Invalid Email or Password!");
+        console.log(error);
+      }
+    );
   }
 }
