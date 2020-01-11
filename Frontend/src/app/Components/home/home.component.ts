e1d56937c1ccd730c980ecfb6b085aa4ffe983b8
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-11 20:05:18 +0530

    #Frontend: like button and counter impl. [To be used in future]

--- Frontend/src/app/Components/home/home.component.ts
+++ Frontend/src/app/Components/home/home.component.ts
@@ -6,7 +6,26 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  public likes: number;
+  public activeHeart: boolean;
   constructor() {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.likes = 0;
+  }
+
+  like() {
+    this.likes++;
+  }
+  dislike() {
+    this.likes--;
+    if (this.likes < 0) {
+      this.likes = 0;
+    }
+    return this.likes;
+  }
+
+  likeHeart() {
+    this.activeHeart = !this.activeHeart;
+  }
 }
