5fb539bd3e39f00191ec9bc1a5c9e732e603cb3f
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-20 18:07:31 +0530

    #MyProfile: Enable WOW animations

--- Frontend/src/app/Components/myprofile/myprofile/myprofile.component.ts
+++ Frontend/src/app/Components/myprofile/myprofile/myprofile.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
 import { userLoginData } from "../../../Shared/Services/userLoginService";
 import { IuserReg } from "../../../Shared/Model/usersRegistration";
+import { WOW } from "wowjs/dist/wow.min";
 
 @Component({
   selector: "app-myprofile",
@@ -13,6 +14,7 @@ export class MyprofileComponent implements OnInit {
   constructor(private UL: userLoginData) {}
 
   ngOnInit() {
+    new WOW({ live: false }).init(); //enable wow animations
     this.UL.myProfile().subscribe(item => {
       this.arrivedUsers = item;
       console.log(item);
