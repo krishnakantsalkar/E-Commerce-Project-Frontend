4967226bb2fd798aa1c9d322ea1026e7a33e5b4c
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-23 16:33:42 +0530

    #Products: Add Search Filter Pipe

--- Frontend/src/app/Components/products/products.module.ts
+++ Frontend/src/app/Components/products/products.module.ts
@@ -3,9 +3,16 @@ import { CommonModule } from "@angular/common";
 import { ProductsComponent } from "./products/products.component";
 import { RouterModule } from "@angular/router";
 import { Products } from "./products-routes";
+import { FormsModule } from "@angular/forms";
+import { Ng2SearchPipeModule } from "ng2-search-filter";
 
 @NgModule({
   declarations: [ProductsComponent],
-  imports: [CommonModule, RouterModule.forChild(Products)]
+  imports: [
+    CommonModule,
+    RouterModule.forChild(Products),
+    FormsModule,
+    Ng2SearchPipeModule
+  ]
 })
 export class ProductsModule {}
