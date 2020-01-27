8cdce121a8504beb0f521a672b517c0ec27af096
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-01-27 22:11:23 +0530

    #Cart: Implement Cart Functionality and display items added in Cart

--- Frontend/src/app/Components/floating-cart/floating-cart.component.ts
+++ Frontend/src/app/Components/floating-cart/floating-cart.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-floating-cart',
-  templateUrl: './floating-cart.component.html',
-  styleUrls: ['./floating-cart.component.css']
+  selector: "app-floating-cart",
+  templateUrl: "./floating-cart.component.html",
+  styleUrls: ["./floating-cart.component.css"]
 })
 export class FloatingCartComponent implements OnInit {
-
-  constructor() { }
+  public cartItems;
+  public cartCount;
+  public count: number;
+  constructor() {}
 
   ngOnInit() {
+    this.cartItems = JSON.parse(localStorage.getItem("product"));
+    console.log(this.cartItems);
+    this.count = this.cartItems.length;
   }
 
+  removefromCart() {
+    localStorage.removeItem("product");
+    location.reload();
+  }
 }
