49fe63f7531bda81f41a15d7693a13d4d1d3c7af
krishnakantsalkar <krishnakantsalkar@gmail.com>
2020-03-31 21:53:47 +0530

    #Frontend: Remove RSS feed because the npm dependancy became deprecated
    Signed-off-by: krishnakantsalkar <krishnakantsalkar@gmail.com>

--- Frontend/src/app/Components/rssfeed/rssfeed-routes.ts
+++
@@ -1,9 +0,0 @@
-import { Route } from "@angular/router";
-import { RssfeedComponent } from "./rssfeed/rssfeed.component";
-
-export const rssfeed: Route[] = [     // child routing and lazy loading
-  {
-    path: "",
-    component: RssfeedComponent
-  }
-];
